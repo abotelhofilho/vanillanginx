624c708ecbd88552b6112c18adcfc0e39e3f0846
Antonino BotelhoFilho <abotelhofilho@users.noreply.github.com>
2023-11-02 15:00:14 -0400

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,4 +1,6 @@
 apt-get update
 apt-get install -y nginx
 echo $(hostname) | sudo tee /var/www/html/index.html
+echo "<br>" | sudo tee -a /var/www/html/index.html
 echo "<img src="https://azure.microsoft.com/en-us/blog/wp-content/uploads/2021/05/95baa365-fedb-4d3c-8b1f-22735e3bb77a.webp">" | sudo tee -a /var/www/html/index.html
+echo "<br>" | sudo tee -a /var/www/html/index.html
